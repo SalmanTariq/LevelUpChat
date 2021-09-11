b9df4f6573c023c61cff72b3a08d22f732b7f670
Salman <salman.tariq90@gmail.com>
2021-09-11 08:32:27 +0500

    formats date to distance

--- resources/js/Pages/Chat/Components/MessageItem.vue
+++ resources/js/Pages/Chat/Components/MessageItem.vue
@@ -1,32 +1,29 @@
 <template>
-  <div>
+    <div>
         <div class="flex space-x-3 px-3">
-          <img class="mt-4 h-8 w-8 rounded-full" :src="message.user.profile_photo_url" :alt="message.user.name" />
-          <div class="flex-1 space-y-1">
-            <div class="flex items-center justify-between">
-              <h3 class="pt-2 text-sm font-medium">{{ message.user.name }}</h3>
-              <p class="text-sm text-gray-500">{{ readableDate(message.created_at) }}</p>
+            <img class="mt-4 h-8 w-8 rounded-full" :src="message.user.profile_photo_url" :alt="message.user.name"/>
+            <div class="flex-1 space-y-1">
+                <div class="flex items-center justify-between">
+                    <h3 class="pt-2 text-sm font-medium">{{ message.user.name }}</h3>
+                    <p class="text-sm text-gray-500">{{ readableDate(message.created_at) }}</p>
+                </div>
+                <p class="text-md text-gray-500">{{ message.message }}</p>
             </div>
-            <p class="text-md text-gray-500">{{ message.message }}</p>
-          </div>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
-import { TimeAgo } from 'vue2-timeago'
+import {formatDistanceToNow} from 'date-fns'
 
 export default {
-  components: {
-      TimeAgo,
-  },
-  props: ['message'],
+    props: ['message'],
 
-  methods: {
-    readableDate (date) {
-        console.log(this.$page)
-      return (new Date(date)).toLocaleString()
+    methods: {
+        readableDate(date) {
+            const distance = formatDistanceToNow(new Date(date));
+            return distance === 'less than a minute' ? 'just now' : distance;
+        }
     }
-  }
 }
 </script>
